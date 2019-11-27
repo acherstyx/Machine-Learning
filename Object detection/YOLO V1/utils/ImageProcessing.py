c1df0ab198c4654b352d6c0b4d5099e989bd40fa
AcherStyx <SYJ.Micro@outlook.com>
2019-11-28 00:04:32 +0800

    [update] Rewrite test case for Pascal VOC data-loader and draw bbox

--- Object detection/YOLO V1/utils/ImageProcessing.py
+++ Object detection/YOLO V1/utils/ImageProcessing.py
@@ -4,127 +4,137 @@ import utils.Config as Config
 from math import sqrt
 
 
-def DrawBoundingBox(predict_result,
-                    current_image,
-                    bbox_num,
-                    base_coordinate,
+def DrawBoundingBox(label_array,
+                    image_to_process,
+                    bbox_number,
+                    base_coordinate_of_xy,
                     is_logits,
                     threshold=0.8,
                     color=(0, 255, 0)):
     """
-
-    @param predict_result: the information of boundary box
-    @param current_image: image
-    @param bbox_num: number of boundary box
-    @param base_coordinate: "IMAGE" based or "CELL" based
+    Draw the bounding box based on the predict result or label
+    Note that the function do 'in-place editing' to the image
+    @param label_array: the information of boundary box
+    [bbox:{confidence, x, y, w, h} * bbox_number, logits/index of predict result]
+    @param image_to_process: image
+    @param bbox_number: number of boundary box
+    @param base_coordinate_of_xy: "IMAGE" based or "CELL" based
+    whether to add offset to xy coordinate
     @param is_logits: the format of class label, True or False
-    @param threshold:
-    @param color:
+    @param threshold: the bbox will only be drawn on the image if the confidence of bbox is over threshold
+    @param color: the color of box and text ( , , )
     @return: image
     """
-    for rows, i in enumerate(predict_result):  # row
+    for rows, i in enumerate(label_array):  # row
         for cols, ii in enumerate(i):  # col
-            # [confidence,confidence,x,y,w,h,x,y,w,h] x,y is cell-based w,h is image-based
-            if np.amax(ii[:bbox_num]) < threshold:
+            # [confidence,confidence,x,y,w,h,x,y,w,h] x,y is cell-based/image-based, w,h is all image-based
+            if np.amax(ii[:bbox_number]) < threshold:
                 continue
             else:
-                box_index = np.argmax(ii[:bbox_num])
-                offset = box_index * 4 + bbox_num
+                box_index = np.argmax(ii[:bbox_number])
+                offset = bbox_number + box_index * 4
 
                 if Config.DebugOutput_ImageShow_Point:
-                    print("Point info in DrawBoundingBox() - 1:", base_coordinate, ii[offset:offset + 4])
+                    print("Point info in DrawBoundingBox() - 1:", base_coordinate_of_xy, ii[offset:offset + 4])
 
-                if base_coordinate == "CELL":
+                if base_coordinate_of_xy == "CELL":
+                    # add offset to the image
                     center_x = int((ii[offset + 0] + cols) / Config.CellSize * Config.ImageSize)
                     center_y = int((ii[offset + 1] + rows) / Config.CellSize * Config.ImageSize)
-                elif base_coordinate == "IMAGE":
+                    # use square of w,h as the real w,h
+                    w = int((ii[offset + 2] ** 2) * Config.ImageSize)
+                    h = int((ii[offset + 3] ** 2) * Config.ImageSize)
+                elif base_coordinate_of_xy == "IMAGE":
                     center_x = int(ii[offset + 0] * Config.ImageSize)
                     center_y = int(ii[offset + 1] * Config.ImageSize)
+                    w = int(ii[offset + 2] * Config.ImageSize)
+                    h = int(ii[offset + 3] * Config.ImageSize)
                 else:
                     raise AttributeError
 
-                w = int(ii[offset + 2] * Config.ImageSize)
-                h = int(ii[offset + 3] * Config.ImageSize)
-
+                # left up point
                 x1 = int(center_x - w / 2)
                 y1 = int(center_y - h / 2)
+                # right down point
                 x2 = int(center_x + w / 2)
                 y2 = int(center_y + h / 2)
 
                 if Config.DebugOutput_ImageShow_Point:
                     print("Point info in DrawBoundingBox() - 2:", x1, y1, x2, y2)
 
-                cv.rectangle(current_image,
-                             (x1, y1),
-                             (x2, y2),
+                cv.rectangle(img=image_to_process,
+                             pt1=(x1, y1),
+                             pt2=(x2, y2),
                              color=color,
                              thickness=1)
 
                 if is_logits:
-                    class_index = np.argmax(ii[5 * bbox_num:])
+                    class_index = np.argmax(ii[5 * bbox_number:])
                 else:
-                    class_index = ii[5 * bbox_num]
-                cv.putText(current_image,
-                           Config.Classes[int(class_index)] + " {:.2f}".format(np.amax(ii[:bbox_num])),
-                           (x1 + 10, y1 + 15),
-                           cv.FONT_HERSHEY_SIMPLEX,
-                           0.5,
-                           color)
+                    class_index = ii[5 * bbox_number]
+                cv.putText(img=image_to_process,
+                           text=Config.Classes[int(class_index)] + " {:.2f}".format(np.amax(ii[:bbox_number])),
+                           org=(x1 + 10, y1 + 15),
+                           fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                           fontScale=0.5,
+                           color=color)
 
                 if Config.DebugOutput_ImageShow_Logits:
-                    print("Logits info in DrawBoundingBox():", ii[5 * bbox_num:])
+                    print("Logits info in DrawBoundingBox():", ii[5 * bbox_number:])
 
-    print("====================")
-    return current_image
+    return image_to_process
 
 
 if __name__ == "__main__":
-    import utils.LoadPascalVOC as voc
+    # TODO: create new test case
 
-    data = voc.PascalVOC()
+    # load image data from Pascal VOC data set
+    import utils.LoadPascalVOC as LoadPascalVoc
 
-    iter = data.train_generator(1)
-    for image, label in iter:
-        image = image["input"]
-        label = label["output"]
-        result = np.zeros([7, 7, 30])
-        result[1, 1, :] = [0.5, 0.99,
-                           0.99, 0.99, 0.1, 0.1,
-                           0.5, 0.5, 0.1, 0.1,
-                           0.5, 0.5, 0.4, 0.8, 0.9, 0.6, 0.4, 0.4, 0.4, 0.5,
-                           0.55, 0.88, 0.99, 0.99, 0.14, 0.66, 0.54, 0.55, 0.66, 0.55]
-        image_result = DrawBoundingBox(result, image[0], 2, "CELL", True, 0.8)
-        cv.imshow("out", image_result)
-        cv.waitKey()
-        break
+    # create data loader
+    data_loader = LoadPascalVoc.PascalVOC()
+    # get train data generator
+    train_iter = data_loader.train_generator(1)
+    # get valid data generator
+    valid_iter = data_loader.valid_generator(1)
 
-    iter = data.train_generator(1)
-    for image, label in iter:
-        image = image["input"][0]
-        label = label["output"][0]
+    # draw bbox based on label data
+    print(">>> Draw bbox based on label data")
+    current_image, current_label = train_iter.__next__()
+    current_image = current_image["input"][0]
+    current_label = current_label["output"][0]
+    # get labeled image
+    labeled_image = DrawBoundingBox(label_array=current_label,
+                                    image_to_process=current_image.copy(),
+                                    bbox_number=1,
+                                    base_coordinate_of_xy="IMAGE",
+                                    is_logits=False,
+                                    threshold=1.0,
+                                    color=(255,255,255),
+                                    )
+    # show image
+    cv.imshow("Deal with label from dataset",np.hstack((current_image,labeled_image)))
+    cv.waitKey(2000)
 
-        image_result = DrawBoundingBox(label, image, 1, "IMAGE", False)
-        cv.imshow("origin", image_result)
-
-        result = np.zeros([7, 7, 30])
-        result[:, :, 0] = result[:, :, 1] = label[:, :, 0]
-        result[:, :, 2:6] = result[:, :, 6:10] = label[:, :, 1:5]
-        image_result = DrawBoundingBox(result, image, 2, "IMAGE", True, 0.8, color=(0, 0, 255))
-        cv.imshow("with logits", image_result)
-        cv.waitKey()
-        break
-
-    iter = data.train_generator(1)
-    for image, label in iter:
-        image = image["input"][0]
-        label = label["output"][0]
-
-        result = label
-        for rows, i in enumerate(result):
-            for cols, ii in enumerate(i):
-                image[rows, cols, 1] = image[rows, cols, 1] * Config.CellSize / Config.ImageSize - cols
-                image[rows, cols, 2] = image[rows, cols, 2] * Config.CellSize / Config.ImageSize - rows
-        image_result = DrawBoundingBox(result, image, 1, "IMAGE", False, color=(255, 0, 0))
-
-        cv.imshow("test", image_result)
-        cv.waitKey()
+    # generate output of neural network based on label
+    print(">>> Show simulate label (logits, xy is image based)")
+    simulate_label = np.zeros([7,7,30],dtype=np.float)
+    simulate_label[...,1] = current_label[...,0]
+    simulate_label[...,6:10] = current_label[...,1:5]
+    for i in range(7):
+        for ii in range(7):
+            if(np.asscalar(current_label[i,ii,0])==1):
+                simulate_label[i,ii,10+int(np.asscalar(current_label[i,ii,5]))] = 1.0
+    # get labeled image
+    labeled_image = DrawBoundingBox(label_array=simulate_label,
+                                    image_to_process=current_image.copy(),
+                                    bbox_number=2,
+                                    base_coordinate_of_xy="IMAGE",
+                                    is_logits=True,
+                                    threshold=1.0,
+                                    color=(0,255,0),
+                                    )
+    # show image
+    cv.imshow("Deal with simulated label",np.hstack((current_image,labeled_image)))
+    cv.waitKey(2000)
+    
--- Object detection/YOLO V1/utils/LoadPascalVOC.py
+++ Object detection/YOLO V1/utils/LoadPascalVOC.py
@@ -1,9 +1,9 @@
 import xml.etree.cElementTree as ET
 import pickle
-import cv2.cv2 as cv
+import cv2 as cv
 import numpy as np
 import random
-from utils import Config as cfg
+from utils import Config
 import os
 
 PASCAL_VOC_PATH = "./.VOCdevkit/"
@@ -13,40 +13,40 @@ class PascalVOC:
     def __init__(self):
         # path
         #   data
-        self.ImagePath = cfg.ImagePath
-        self.AnnotationPath = cfg.AnnotationsPath
+        self.ImagePath = Config.ImagePath
+        self.AnnotationPath = Config.AnnotationsPath
         #   save
-        self.SaveTrain = cfg.TrainSavePath
-        self.SaveVal = cfg.ValSavePath
+        self.SaveTrain = Config.TrainSavePath
+        self.SaveVal = Config.ValSavePath
         #   image list
         self.ImageList = os.listdir(self.ImagePath)
         self.ImageList = [i.replace(".jpg", "") for i in self.ImageList]
         random.shuffle(self.ImageList)
 
         # image settings
-        self.ImageSize = cfg.ImageSize
-        self.CellSize = cfg.CellSize
+        self.ImageSize = Config.ImageSize
+        self.CellSize = Config.CellSize
 
         # class
-        self.Classes = cfg.Classes
-        self.ClassesDict = cfg.ClassesDict
+        self.Classes = Config.Classes
+        self.ClassesDict = Config.ClassesDict
 
         # divide data set to train and val
-        self.TrainPercentage = cfg.TrainPercentage
+        self.TrainPercentage = Config.TrainPercentage
         self.TrainNum = int(len(self.ImageList) * self.TrainPercentage)
         self.ValNum = len(self.ImageList) - self.TrainNum
         self.TrainImageList = self.ImageList[:self.TrainNum]
         self.ValImageList = self.ImageList[self.TrainNum:]
 
         # start loading data
-        if cfg.LoadSavedData and os.path.isfile(self.SaveTrain) and os.path.isfile(self.SaveVal):
-            self.TrainData = self.__LoadPickle__(self.SaveTrain)
-            self.ValData = self.__LoadPickle__(self.SaveVal)
+        if Config.LoadSavedData and os.path.isfile(self.SaveTrain) and os.path.isfile(self.SaveVal):
+            self.TrainData = self.__LoadFromPickle__(self.SaveTrain)
+            self.ValidData = self.__LoadFromPickle__(self.SaveVal)
         else:
             # load label
             self.__LoadImageLabel__()
             self.__SaveToPickle__(self.SaveTrain, self.TrainData)
-            self.__SaveToPickle__(self.SaveVal, self.ValData)
+            self.__SaveToPickle__(self.SaveVal, self.ValidData)
 
     # accelerated loading save
     @staticmethod
@@ -57,14 +57,14 @@ class PascalVOC:
 
     # accelerated loading load
     @staticmethod
-    def __LoadPickle__(file_path):
+    def __LoadFromPickle__(file_path):
         print("Load data from cache: " + file_path)
         with open(file_path, "rb") as f:
             return pickle.load(f)
 
     # load from raw pascal voc data
     def __LoadImageLabel__(self):
-        print("Loading labels from: " + self.AnnotationPath)
+        print("Loading labels from annotation directory: " + self.AnnotationPath)
 
         self.TrainData = []
         # load train data set
@@ -74,14 +74,15 @@ class PascalVOC:
                 continue
             current_image_path = os.path.join(self.ImagePath, TrainImage + ".jpg")
             self.TrainData.append({"ImagePath": current_image_path, "Label": current_label})
-        self.ValData = []
+
+        self.ValidData = []
         # load val data set
         for ValImage in self.ValImageList:
             current_label, has_object = self.__LoadLabel__(ValImage)
             if has_object == 0:
                 continue
             current_image_path = os.path.join(self.ImagePath, ValImage + ".jpg")
-            self.ValData.append({"ImagePath": current_image_path, "Label": current_label})
+            self.ValidData.append({"ImagePath": current_image_path, "Label": current_label})
 
     def __LoadLabel__(self, image_name):
         annotation_path = os.path.join(self.AnnotationPath, image_name + ".xml")
@@ -119,7 +120,7 @@ class PascalVOC:
                 object_counter += 1
             # format: confidence[0] x,y,w,h[0:4] classes[5]
             label_temp[cell_y, cell_x, 0] = 1
-            label_temp[cell_y, cell_x, 1:5] = [i / cfg.ImageSize for i in box_info]
+            label_temp[cell_y, cell_x, 1:5] = [i / Config.ImageSize for i in box_info]
             label_temp[cell_y, cell_x, 5] = class_id
 
         return label_temp, object_counter
@@ -131,19 +132,21 @@ class PascalVOC:
                 batch_label = []
                 batch_data = self.TrainData[i - batch_size: i]
                 for single_sample in batch_data:
-                    batch_img.append(cv.resize(cv.imread(single_sample["ImagePath"]), (cfg.ImageSize, cfg.ImageSize)))
+                    batch_img.append(
+                        cv.resize(cv.imread(single_sample["ImagePath"]), (Config.ImageSize, Config.ImageSize)))
                     batch_label.append(single_sample["Label"])
                 yield {"input": np.array(batch_img, dtype=np.float) / 255.0}, \
                       {"output": np.array(batch_label, dtype=np.float)}
 
-    def val_generator(self, batch_size):
+    def valid_generator(self, batch_size):
         while True:
             for i in range(batch_size, self.ValNum, batch_size):
                 batch_img = []
                 batch_label = []
-                batch_data = self.ValData[i - batch_size: i]
+                batch_data = self.ValidData[i - batch_size: i]
                 for single_sample in batch_data:
-                    batch_img.append(cv.resize(cv.imread(single_sample["ImagePath"]), (cfg.ImageSize, cfg.ImageSize)))
+                    batch_img.append(
+                        cv.resize(cv.imread(single_sample["ImagePath"]), (Config.ImageSize, Config.ImageSize)))
                     batch_label.append(single_sample["Label"])
                 batch_img = np.array(batch_img)
                 batch_label = np.array(batch_label)
@@ -154,41 +157,110 @@ class PascalVOC:
 if __name__ == "__main__":
     data = PascalVOC()
 
-    val_iter = data.val_generator(10)
-
+    # test val generator
+    print(">>> Test output shape of validation data generator")
+    # get generator
+    val_iter = data.valid_generator(10)
+    # print the data shape of validation generator
     for image, label in val_iter:
         print("Data shape:", np.shape(image["input"]), np.shape(label["output"]))
-        print(label)
-        print(image)
         break
 
-    # get image sample
-    index = int(input("Index of image to show: "))
-    while True:
+    # test the amount of train and validation data
+    print(">>> Test the amount of train and validation data")
+    print("The length of train data is:", len(data.TrainData))
+    print("The length of valid data is:", len(data.ValidData))
+
+    # get image sample from train generator
+    print(">>> Show sample image of train data without using generator")
+    index = random.randint(0, len(data.TrainData) - 11)
+    print("Index of image to show:", index)
+    # show 10 sample image
+    for _ in range(10):
+        # dictionary of data
         sample = data.TrainData[index]
+        # separate data from the dict
         image_path = sample["ImagePath"]
         label = sample["Label"]
-
+        # read image
         image = cv.imread(image_path)
-        image = cv.resize(image, (cfg.ImageSize, cfg.ImageSize))
-        cv.putText(image,
-                   str(index) + image_path,
-                   (0, 12),
-                   cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-        cv.imshow("InitImage", image)
+        # resize to the shape used in neural network model
+        image = cv.resize(image, (Config.ImageSize, Config.ImageSize))
+        # make a copy
+        image_old = image.copy()
+        # use label to add bbox and class name
         for i in label:
             for ii in i:
                 if ii[0] != 0.0:
+                    # draw bound box on image
                     cv.rectangle(image,
-                                 (int((ii[1] - ii[3] / 2) * cfg.ImageSize), int((ii[2] - ii[4] / 2) * cfg.ImageSize)),
-                                 (int((ii[1] + ii[3] / 2) * cfg.ImageSize), int((ii[2] + ii[4] / 2) * cfg.ImageSize)),
+                                 (int((ii[1] - ii[3] / 2) * Config.ImageSize),
+                                  int((ii[2] - ii[4] / 2) * Config.ImageSize)),
+                                 (int((ii[1] + ii[3] / 2) * Config.ImageSize),
+                                  int((ii[2] + ii[4] / 2) * Config.ImageSize)),
                                  (0, 255, 0),
                                  2)
+                    # put class name on image
                     cv.putText(image,
-                               cfg.Classes[int(ii[5])],
-                               (int((ii[1] - ii[3] / 2) * cfg.ImageSize + 2),
-                                int((ii[2] - ii[4] / 2) * cfg.ImageSize + 12)),
-                               cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-        cv.imshow("LabeledImage", image)
-        cv.waitKey()
+                               Config.Classes[int(ii[5])],
+                               (int((ii[1] - ii[3] / 2) * Config.ImageSize + 2),
+                                int((ii[2] - ii[4] / 2) * Config.ImageSize + 15)),
+                               cv.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 0))
+        # stack labeled and unlabeled image
+        image = np.hstack((image_old, image))
+        # put the path info of the image
+        cv.putText(image,
+                   "index: " + str(index) + " path: " + image_path,
+                   (0, 12),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+        cv.imshow("Labeled Train Image", image)
+        cv.waitKey(500)
+        # update index of the showing image
+        index += 1
+
+    # get image sample from valid generator
+    print(">>> Show sample image of valid data without using generator")
+    index = random.randint(0, len(data.ValidData) - 11)
+    print("Index of image to show:", index)
+    # show 10 sample image
+    for _ in range(10):
+        # dictionary of data
+        sample = data.ValidData[index]
+        # separate data from the dict
+        image_path = sample["ImagePath"]
+        label = sample["Label"]
+        # read image
+        image = cv.imread(image_path)
+        # resize to the shape used in neural network model
+        image = cv.resize(image, (Config.ImageSize, Config.ImageSize))
+        # make a copy
+        image_old = image.copy()
+        # use label to add bbox and class name
+        for i in label:
+            for ii in i:
+                if ii[0] != 0.0:
+                    # draw bound box on image
+                    cv.rectangle(image,
+                                 (int((ii[1] - ii[3] / 2) * Config.ImageSize),
+                                  int((ii[2] - ii[4] / 2) * Config.ImageSize)),
+                                 (int((ii[1] + ii[3] / 2) * Config.ImageSize),
+                                  int((ii[2] + ii[4] / 2) * Config.ImageSize)),
+                                 (0, 255, 0),
+                                 2)
+                    # put class name on image
+                    cv.putText(image,
+                               Config.Classes[int(ii[5])],
+                               (int((ii[1] - ii[3] / 2) * Config.ImageSize + 2),
+                                int((ii[2] - ii[4] / 2) * Config.ImageSize + 15)),
+                               cv.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 0))
+        # stack labeled and unlabeled image
+        image = np.hstack((image_old, image))
+        # put the path info of the image
+        cv.putText(image,
+                   "index: " + str(index) + " path: " + image_path,
+                   (0, 12),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+        cv.imshow("Labeled Valid Image", image)
+        cv.waitKey(500)
+        # update index of the showing image
         index += 1
