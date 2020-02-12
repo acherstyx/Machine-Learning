c274f5538e8ce0c836d4df08669e95ee402457df
AcherStyx <SYJ.Micro@outlook.com>
2020-02-13 06:09:27 +0800

    [feat] Add training process

--- Text/Text generation/train.py
+++ Text/Text generation/train.py
@@ -1,4 +1,23 @@
+import os
+# close warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from utils import *
+from config import *  # load CONST
+from model import simple_gru as rnn_model
 import tensorflow as tf
-import tensorflow.keras.layers as layers
-import utils.load_data as load_data
 
+dataset = load_data.sample_dataset.shuffle(buffer_size=SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+vec2word = load_data.sample_dict
+
+model = rnn_model.build_model(len(vec2word), 256, 1024, 64)
+
+# for input, label in dataset.take(1):
+#     output = model(input)
+#     print(output)
+
+callback_checkpoint = tf.keras.callbacks.ModelCheckpoint(CHECKPOINT_PATH,
+                                                         save_weights_only=True)
+
+history = model.fit(dataset,
+                    epochs=EPOCHS,
+                    callbacks=[callback_checkpoint])
